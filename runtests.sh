affdd14a11cf515e32cfc5b343f4d79cc18e5a90
Reno Reckling <reno.reckling@mayflower.de>
2013-03-17 15:46:48 +0100

    fix bugs and extend rsync tests

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-rm -fr /lib/test/testDir2/* 2>/dev/null
 rm -fr instrumented/* 2>/dev/null
 rm coverage.html 2>/dev/null
 jscover lib instrumented && vows --cover-html --spec instrumented/test/*.js
